bc7211ef74774622e66ce90c928500c95a5e2b63
latifaheydari <latifa.heydari@students.plymouth.ac.uk>
2022-10-27 16:19:08 +0100

    git

--- Task-101-Blinky/main.cpp
+++ Task-101-Blinky/main.cpp
@@ -2,12 +2,12 @@
 
 // Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
 DigitalOut greenLED(LED1);
-DigitalOut redLED   (LED2);
-DigitalOut blueLED  (LED3);
+
+//DigitalOut blueLED  (LED3);
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut redLED(LED2);
-//DigitalOut blueLED(LED3);
+DigitalOut redLED (LED2);
+DigitalOut blueLED (LED3);
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
@@ -19,13 +19,13 @@ int main()
 
         // Turn onboard LED ON  
         greenLED = 1; 
-        redLED   = 0;
+        redLED   = 1;
         blueLED  = 0;
         // Wait 0.5 second (1 million microseconds)
         wait_us(200000); 
 
         // Turn  onboard LED ON
-        greenLED = 0;
+        greenLED = 1;
         redLED   = 1;
         blueLED  = 0;
         // Wait 0.5 second
--- Task-102-SOS/main.cpp
+++ Task-102-SOS/main.cpp
@@ -7,6 +7,10 @@ DigitalOut greenLED(TRAF_GRN1_PIN);
 Buzzer buzz;
 Buttons buttons;
 
+void S();
+void SS();
+void O();
+void letter_space();
 // TIP: (I suggest you read this!)
 //
 // Press the black reset button to restart the code (and stop the sound)
@@ -18,22 +22,197 @@ int main()
     while (buttons.BlueButton == 0);
     
     //Repeat everything "forever" (until the power is removed or the chip is reset)
+    while(true){
+        SS();
+        letter_space();
+        O();
+        letter_space();
+        SS();
+        letter_space();
+        letter_space();
+    }
+    
+    
+    
+    
     while (true)
     {
         //On for 500ms
         greenLED = 1;
-        buzz.playTone("C");
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        buzz.playTone("c");
+        wait_us(1000 * 150);  //500ms
 
 
         //Off for 500ms
         greenLED = 0;
-        buzz.playTone("C", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        buzz.rest();
+        wait_us(1000 * 150);  //500ms
+
+     //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 150);  //500ms
+
+     //Off for 500ms
+        greenLED = 0;
+        buzz.rest ();
+        wait_us(1000 * 150);  //500ms
+
+ //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000* 150);  //500ms
+
+//Off for 500ms  
+        greenLED = 0;
+        buzz.rest ();
+        wait_us(1000* 450);  //500ms
+
+        //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 450);  //500ms
+
+
+        //Off for 500ms  
+        greenLED = 0;
+        buzz.rest ();
+        wait_us(1000 * 150);  //500ms
+
+
+        //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 450);  //500ms
+
+
+
+     //Off for 500ms  
+        greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 150);  //500ms
+
+         //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 450);  //500ms
+
+        //Off for 500ms  
+        greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 450);  //500ms
+
+        //on for 500ms
+         greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 150);  //500ms
+
+
+        //off for 500ms
+         greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 150);  //500ms
+
+         //on for 500ms
+         greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 150); // 500ms 
+
+        //off for 500ms
+         greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 150);  //500ms
+
+        //on for 500ms
+         greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 150); // 500ms
+
 
         //Pause
         buzz.rest();
         wait_us(WAIT_TIME_MS * 1000);
 
     }
+}
+
+void S(){
+ //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 150);  //500ms
+
+
+        //Off for 500ms
+        greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 150);  //500ms
+
+     //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 150);  //500ms
+
+     //Off for 500ms
+        greenLED = 0;
+        buzz.rest ();
+        wait_us(1000 * 150);  //500ms
+
+ //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000* 150);  //500ms
+}
+void SS(){
+    for(int i =0 ; i<3 ; i++){
+         //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 150);  //500ms
+
+        if(i<2){
+        //Off for 500ms
+        greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 150);  //500ms
+        }
+ 
+    }
+}
+void O(){
+ //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 450);  //500ms
+
+
+        //Off for 500ms  
+        greenLED = 0;
+        buzz.rest ();
+        wait_us(1000 * 150);  //500ms
+
+
+        //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 450);  //500ms
+
+
+
+     //Off for 500ms  
+        greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 150);  //500ms
+
+         //On for 500ms
+        greenLED = 1;
+        buzz.playTone("c");
+        wait_us(1000 * 450);  //500ms
+}
+
+void letter_space(){
+         //Off for 500ms  
+        greenLED = 0;
+        buzz.rest();
+        wait_us(1000 * 450);  //500ms
 }
